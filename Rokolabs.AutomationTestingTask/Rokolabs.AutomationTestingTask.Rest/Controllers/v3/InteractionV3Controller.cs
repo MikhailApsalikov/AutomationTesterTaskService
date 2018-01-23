21f4d5532d67155e34b494a713fbfa59d4825316
Mikhail Apsalikov <dee_jay@bk.ru>
2018-01-23 15:17:01 +0400

    Interaction/Event service is implemented for Sprint 3

---
+++ Rokolabs.AutomationTestingTask/Rokolabs.AutomationTestingTask.Rest/Controllers/v3/InteractionV3Controller.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using Rokolabs.AutomationTestingTask.Common;
+using Rokolabs.AutomationTestingTask.Entities;
+using Rokolabs.AutomationTestingTask.Repositories;
+using Rokolabs.AutomationTestingTask.Rest.Models;
+
+namespace Rokolabs.AutomationTestingTask.Rest.Controllers.v3
+{
+	[Route("v3/Interaction")]
+	public class InteractionV3Controller : ApiController
+	{
+		[HttpDelete]
+		public IHttpActionResult Delete(int id, string sessionId)
+		{
+			var repository = EventRepositoryCache.Instance.Get(sessionId.ToGuidWithAccessDenied());
+			var item = repository.GetById(id);
+			if (item == null || !repository.IsInteraction(item))
+			{
+				return InternalServerError();
+			}
+			repository.Delete(id);
+			return Ok();
+		}
+
+		[HttpGet]
+		public IHttpActionResult Get(int id, string sessionId)
+		{
+			EventRepository repository;
+			try
+			{
+				repository = EventRepositoryCache.Instance.Get(sessionId.ToGuidWithAccessDenied());
+			}
+			catch (AccessViolationException)
+			{
+				repository = EventRepositoryCache.Instance.BrokenGet();
+			}
+			var item = repository.GetById(id);
+			if (item != null && repository.IsInteraction(item))
+			{
+				return Ok(item);
+			}
+			return NotFound();
+		}
+
+		[HttpGet]
+		public IHttpActionResult Get(string sessionId)
+		{
+			var repository = EventRepositoryCache.Instance.Get(sessionId.ToGuidWithAccessDenied());
+			var items = repository.GetAll().Where(e => repository.IsInteraction(e));
+			return Ok(items);
+		}
+
+		[HttpPost]
+		public IHttpActionResult Post(string sessionId, [FromBody] Event value)
+		{
+			var repository = EventRepositoryCache.Instance.Get(sessionId.ToGuidWithAccessDenied());
+			var validationResult = EventValidator.ValidateV3Create(value, true);
+			if (!string.IsNullOrWhiteSpace(validationResult))
+			{
+				return InternalServerError(new ArgumentException(validationResult));
+			}
+			var result = repository.Create(value);
+			return Ok(result);
+		}
+
+		[HttpPut]
+		public IHttpActionResult Put(int id, string sessionId, [FromBody] Event value)
+		{
+			var repository = EventRepositoryCache.Instance.Get(sessionId.ToGuidWithAccessDenied());
+			var validationResult = EventValidator.ValidateV3Update(value, true);
+			if (!string.IsNullOrWhiteSpace(validationResult))
+			{
+				return InternalServerError(new ArgumentException(validationResult));
+			}
+			var item = repository.GetById(id);
+			if (item == null || !repository.IsInteraction(item))
+			{
+				return NotFound();
+			}
+			item.Date = value.Date;
+			item.AddressType = value.AddressType;
+			item.Broker = value.Broker;
+			item.BrokerAttendees = value.BrokerAttendees;
+			item.Companies = value.Companies;
+			item.Duration = value.Duration;
+			item.InteractionType = value.InteractionType;
+			item.InvestorAttendees = value.InvestorAttendees;
+			item.Location = value.Location;
+			item.MeetingTypes = value.MeetingTypes;
+			item.Sectors = value.Sectors;
+			item.Title = value.Title;
+			repository.SetUpdated(item);
+			return Ok();
+		}
+	}
+}
--- Rokolabs.AutomationTestingTask/Rokolabs.AutomationTestingTask.Rest/Models/EventValidator.cs
+++ Rokolabs.AutomationTestingTask/Rokolabs.AutomationTestingTask.Rest/Models/EventValidator.cs
@@ -128,5 +128,95 @@ namespace Rokolabs.AutomationTestingTask.Rest.Models
 			}
 			return string.Empty;
 		}
+
+		public static string ValidateV3Create(Event e, bool isInteraction)
+		{
+			if (e.Duration < 1)
+			{
+				return "Duration cannot be negative";
+			}
+			if (string.IsNullOrWhiteSpace(e.Title))
+			{
+				return "Title cannot be empty";
+			}
+			if (e.Title.Length < 12)
+			{
+				return "Title cannot have less than 12 symbols";
+			}
+			if (e.Title.ToCharArray().All(Char.IsLower))
+			{
+				return "Title can contain only lower letters";
+			}
+			if (string.IsNullOrWhiteSpace(e.Broker))
+			{
+				return "Broker cannot be empty";
+			}
+			if (e.Broker.Length > 50)
+			{
+				return "Broker cannot have more than 50 symbols";
+			}
+			if (e.Location == null)
+			{
+				return "Location cannot be null";
+			}
+			if (string.IsNullOrWhiteSpace(e.Location.City) && string.IsNullOrWhiteSpace(e.Location.Country))
+			{
+				return "Location cannot be empty";
+			}
+			if (e.Date < DateTime.Now && !isInteraction)
+			{
+				return "Event cannot have date in the past. Please, create an Interaction";
+			}
+			if (e.Date > DateTime.Now && isInteraction)
+			{
+				return "Interaction cannot have date in the future. Please, create an Event";
+			}
+			return string.Empty;
+		}
+
+		public static string ValidateV3Update(Event e, bool isInteraction)
+		{
+			if (e.Duration < 0)
+			{
+				return "Duration cannot be negative";
+			}
+			if (string.IsNullOrWhiteSpace(e.Title))
+			{
+				return "Title cannot be empty";
+			}
+			if (e.Title.Length < 24)
+			{
+				return "Title cannot have less than 24 symbols";
+			}
+			if (e.Title.ToCharArray().All(Char.IsLower))
+			{
+				return "Title can contain only lower letters";
+			}
+			if (string.IsNullOrWhiteSpace(e.Broker))
+			{
+				return "Broker cannot be empty";
+			}
+			if (e.Broker.Length > 50)
+			{
+				return "Broker cannot have more than 50 symbols";
+			}
+			if (e.Location == null)
+			{
+				return "Location cannot be null";
+			}
+			if (string.IsNullOrWhiteSpace(e.Location.City) && string.IsNullOrWhiteSpace(e.Location.Country))
+			{
+				return "Location cannot be empty";
+			}
+			if (e.Date < DateTime.Now && !isInteraction)
+			{
+				return "Event cannot have date in the past. Please, create an Interaction";
+			}
+			if (e.Date > DateTime.Now && isInteraction)
+			{
+				return "Interaction cannot have date in the future. Please, create an Event";
+			}
+			return string.Empty;
+		}
 	}
 }
